a45d23562309bc69a15e3f8ede982f5f6d3f6ee5
Javier Romero <romeropaolini@gmail.com>
2018-09-10 12:11:17 +0200

    Create band class and show the band object

--- src/app/bands/bands.component.ts
+++ src/app/bands/bands.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Band } from '../band';
 
 @Component({
   selector: 'app-bands',
   templateUrl: './bands.component.html',
   styleUrls: ['./bands.component.css']
 })
+
 export class BandsComponent implements OnInit {
 
-  band = 'Java and the Scripts';
-
+  band: Band = {
+    id: 1,
+    name: 'Java and the Scripts'
+  };
 
   constructor() {
   }
