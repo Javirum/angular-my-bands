bb0024e93b3b562d30a09c8cc7e55724cfccec49
Javier Romero <romeropaolini@gmail.com>
2018-09-10 11:51:15 +0200

    Add bands component and displaye on homepage

---
+++ src/app/bands/bands.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bands',
+  templateUrl: './bands.component.html',
+  styleUrls: ['./bands.component.css']
+})
+export class BandsComponent implements OnInit {
+
+  band = 'Java and the Scripts';
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
