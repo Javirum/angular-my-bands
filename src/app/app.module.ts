d9643c3bbc79bca3015c4645886713fce3136c36
Javier Romero <romeropaolini@gmail.com>
2018-09-10 12:25:27 +0200

    Edit band name using two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BandsComponent } from './bands/bands.component';
@@ -10,7 +11,8 @@ import { BandsComponent } from './bands/bands.component';
     BandsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
