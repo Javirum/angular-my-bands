89c763a77345b6d17fa3bc3036fe00588f04b531
Javier Romero <romeropaolini@gmail.com>
2018-09-12 15:53:09 +0200

    Add bands route to routes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { BandsComponent } from './bands/bands.component';
+
+const routes: Routes = [
+  { path: 'bands', component: BandsComponent }
+];
+
 @NgModule({
   exports: [RouterModule]
 })
