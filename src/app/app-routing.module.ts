92b49a9f98f01a1e210d5b8b11ae027711005148
Javier Romero <romeropaolini@gmail.com>
2018-09-13 13:18:30 +0200

    Add band detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { BandsComponent } from './bands/bands.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BandDetailComponent } from './band-detail/band-detail.component';
+
 
 
 const routes: Routes = [
-  { path: 'bands', component: BandsComponent },
-  { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: BandDetailComponent },
+  { path: 'bands', component: BandsComponent },
 ];
 
 @NgModule({
